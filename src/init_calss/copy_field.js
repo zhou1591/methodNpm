c214f6cd5b74b08a8c51ab4e1e9832ccd582a1c7
周靖松 <1402663440@qq.com>
2019-10-05 17:33:16 +0800

    'npm'

--- src/init_calss/copy_field.js
+++ src/init_calss/copy_field.js
@@ -8,7 +8,7 @@
  * @Date: 2019-09-27 18:23:46
  */
 export default function copyField(model) {
-  // 解构，默认目标是form
+  // 解构，默认目标是form 
   let { from, to='form', config } = model;
   // 检查
   if (!from||!config) return new Error('缺少必要参数')
