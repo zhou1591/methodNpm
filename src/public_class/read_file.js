501265527fa0ddd647b4e54802e2886259db2fb5
周靖松 <1402663440@qq.com>
2019-10-06 15:36:08 +0800

    'npm'

--- src/public_class/read_file.js
+++ src/public_class/read_file.js
@@ -3,14 +3,32 @@
  * @information: 最后更新时间
  */
 export default async function readFile(model) {
-    // 获取文件流
-    let file = model.currentTarget.files[0];
-    // 创建FileReader实例
-    let reader = new FileReader();
-    // 读文件
-    await reader.readAsText(file);
-    reader.onload = await function () {
-        // 返回结果
-        return this.result
-    };
+    // 谷歌
+    if (window.FileReader) {
+        // 获取文件流
+        let file = model.currentTarget.files[0];
+        // 创建FileReader实例
+        let reader = new FileReader();
+        // 读文件
+        await reader.readAsText(file);
+        reader.onload = await
+        function() {
+            // 返回结果
+            return this.result
+        };
+    }
+    //支持IE 7 8 9 10
+    else if (typeof window.ActiveXObject != 'undefined') {
+        let xmlDoc;
+        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async = false;
+        xmlDoc.load(model);
+    }
+    //支持FF
+    else if (document.implementation && document.implementation.createDocument) {
+        let xmlDoc;
+        xmlDoc = document.implementation.createDocument("", "", null);
+        xmlDoc.async = false;
+        xmlDoc.load(model);
+    }
 }
