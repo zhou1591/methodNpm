8852142cba939dcafe97cad48cdf700d29ff49ee
周靖松 <1402663440@qq.com>
2019-10-25 15:59:09 +0800

    'npm'

--- src/init_calss/str_encryp.js
+++ src/init_calss/str_encryp.js
@@ -8,10 +8,14 @@
  * @Date: 2019-10-25 14:53:06
  */
 export default function strEncryp(str,start,end){
-    let len =  end - start
-    let endLen = str.length
-    len=len>(endLen-start)?endLen-start:len
     let subStr =''
+    let endLen = str.length
+    let len =  end - start
+    if(end<0){
+        len = endLen + end - 1
+    }
+    len=len>(endLen-start)?endLen-start:len
+    
     while(len--){
         subStr += '*'
     }
