934e49c0d78a2c3468ca6b5ddcf9a78da59db010
周靖松 <1402663440@qq.com>
2020-03-23 13:57:27 +0800

    'npm'

--- src/public_class/read_file.js
+++ src/public_class/read_file.js
@@ -3,28 +3,32 @@
  * @information: 最后更新时间
  */
 export default async function readFile(model) {
-    // 谷歌
-    if (window.FileReader) {
-        // 获取文件流
-        let file = model.currentTarget?model.currentTarget.files[0]:model;
-        // 创建FileReader实例
-        let reader = new FileReader();
-        // 读文件
-        await reader.readAsText(file);
-        return reader
-    }
-    //支持IE 7 8 9 10
-    else if (typeof window.ActiveXObject != 'undefined') {
-        let xmlDoc;
-        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-        xmlDoc.async = false;
-        return xmlDoc.load(model);
-    }
-    //支持FF
-    else if (document.implementation && document.implementation.createDocument) {
-        let xmlDoc;
-        xmlDoc = document.implementation.createDocument("", "", null);
-        xmlDoc.async = false;
-        return xmlDoc.load(model);
-    }
+    return new Promise((resolve, reject) => {
+        // 谷歌
+        if (window.FileReader) {
+            // 获取文件流
+            let file = model.currentTarget ? model.currentTarget.files[0] : model;
+            // 创建FileReader实例
+            let reader = new FileReader();
+            // 读文件
+            await reader.readAsText(file);
+            reader.onload = () => {
+                resolve(reader.result)
+            }
+        }
+        //支持IE 7 8 9 10
+        else if (typeof window.ActiveXObject != 'undefined') {
+            let xmlDoc;
+            xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+            xmlDoc.async = false;
+            resolve(xmlDoc.load(model))
+        }
+        //支持FF
+        else if (document.implementation && document.implementation.createDocument) {
+            let xmlDoc;
+            xmlDoc = document.implementation.createDocument("", "", null);
+            xmlDoc.async = false;
+            resolve(xmlDoc.load(model))
+        }
+    })
 }
